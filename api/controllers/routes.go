098d0ab2e20b4c53a42852bfac8d1a09244aa145
Lucky <ovr-lucky.satriawan@ajaib.co.id>
2022-10-19 21:04:05 +0700

    commit

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -8,16 +8,16 @@ func (s *Server) initializeRoutes() {
 	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(s.Login)).Methods("POST")
 
 	//Items routes
-	s.Router.HandleFunc("/items", middlewares.SetMiddlewareJSON(s.CreateItem)).Methods("POST")
-	s.Router.HandleFunc("/items", middlewares.SetMiddlewareJSON(s.GetItems)).Methods("GET")
-	s.Router.HandleFunc("/items/{id}", middlewares.SetMiddlewareJSON(s.GetItem)).Methods("GET")
+	s.Router.HandleFunc("/items", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.CreateItem))).Methods("POST")
+	s.Router.HandleFunc("/items", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.GetItems))).Methods("GET")
+	s.Router.HandleFunc("/items/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.GetItem))).Methods("GET")
 	s.Router.HandleFunc("/items/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateItem))).Methods("PUT")
 	s.Router.HandleFunc("/items/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.DeleteItem))).Methods("POST")
 
 	//Transactions routes
-	s.Router.HandleFunc("/transactions", middlewares.SetMiddlewareJSON(s.CreateTransaction)).Methods("POST")
-	s.Router.HandleFunc("/transactions", middlewares.SetMiddlewareJSON(s.GetTransactions)).Methods("GET")
-	s.Router.HandleFunc("/transactions/{id}", middlewares.SetMiddlewareJSON(s.GetTransaction)).Methods("GET")
+	s.Router.HandleFunc("/transactions", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.CreateTransaction))).Methods("POST")
+	s.Router.HandleFunc("/transactions", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.GetTransactions))).Methods("GET")
+	s.Router.HandleFunc("/transactions/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.GetTransaction))).Methods("GET")
 	s.Router.HandleFunc("/transactions/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateTransaction))).Methods("PUT")
-	s.Router.HandleFunc("/transactions/{id}", middlewares.SetMiddlewareJSON(s.DeleteTransaction)).Methods("POST")
+	s.Router.HandleFunc("/transactions/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.DeleteTransaction))).Methods("POST")
 }
